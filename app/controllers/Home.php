4dfde7a7bd3ca6c6f807e01df9004569acdb2102
Ryan Adi Saputra <158378604+DreamlandSR@users.noreply.github.com>
2025-01-03 21:50:06 +0700

    Home

---
+++ app/controllers/Home.php
@@ -0,0 +1,52 @@
+<?php
+
+class Home extends Controller {
+    public function index() {
+        $data['judul'] = 'Home';
+        $data['css'] = 'nav.css';
+        $data['js'] = 'script.js';
+        $this->view('templates/header', $data);
+        $this->view('templates/navbar', $data);
+        $this->view('home/index', $data);
+        $this->view('templates/footer', $data);
+    }
+
+    public function login() 
+    {
+        $data['judul'] = 'Login';
+        $data['css'] = 'masuk.css';
+        $this->view('templates/header', $data);
+        $this->view('home/login', $data);
+        $this->view('templates/footer', $data);
+    }
+
+    public function about() {
+        $data['judul'] = 'About';
+        $data['css'] = 'nav.css';
+        $data['js'] = 'script.js';
+        $this->view('templates/header', $data);
+        $this->view('templates/navbar', $data);
+        $this->view('home/about', $data);
+        $this->view('templates/footer', $data);
+    }
+
+    public function guide() {
+        $data['judul'] = 'About';
+        $data['css'] = 'nav.css';
+        $data['js'] = 'script.js';
+        $this->view('templates/header', $data);
+        $this->view('templates/navbar', $data);
+        $this->view('home/guide', $data);
+        $this->view('templates/footer', $data);
+    }
+
+    public function features() {
+        $data['judul'] = 'Features';
+        $data['css'] = 'nav.css';
+        $data['js'] = 'script.js';
+        $this->view('templates/header', $data);
+        $this->view('templates/navbar', $data);
+        $this->view('home/features', $data);
+        $this->view('templates/footer', $data);
+    }
+}
---
+++ app/view/templates/footer.php
@@ -0,0 +1,22 @@
+<footer class="bg-dark py-4 mt-auto">
+    <div class="container px-5">
+        <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+            <div class="col-auto">
+                <div class="small m-0 text-white">Copyright &copy; Your Website 2023</div>
+            </div>
+            <div class="col-auto">
+                <a class="link-light small" href="#!">Privacy</a>
+                <span class="text-white mx-1">&middot;</span>
+                <a class="link-light small" href="#!">Terms</a>
+                <span class="text-white mx-1">&middot;</span>
+                <a class="link-light small" href="#!">Contact</a>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Bootstrap core JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap.js" crossorigin="anonymous"></script>
+</body>
+</html>
